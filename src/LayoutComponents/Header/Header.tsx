00bf9868728d2dfde9d27e72611dfc9d9966521f
nabil <sarzanovnabil108@gmail.com>
2024-08-22 08:34:10 +0600

    fix: fixed Header

--- src/LayoutComponents/Header/Header.tsx
+++ src/LayoutComponents/Header/Header.tsx
@@ -1,21 +1,35 @@
+import { useEffect, useRef, useState } from 'react';
 import styles from './Header.module.css';
-import {FaPhone} from 'react-icons/fa';
-import {CiMail, CiSearch} from 'react-icons/ci';
-import {HiOutlineQuestionMarkCircle} from 'react-icons/hi';
-import {FaInstagram, FaWhatsapp, FaYoutube} from 'react-icons/fa';
+import Menu from '../../components/Menu/Menu.tsx';
 import Logo from '../../assets/Images/GreenLogo.jpg';
-import {IoMdArrowDropdown} from 'react-icons/io';
-import  {useState} from "react";
-import Menu from '../../components/Menu/Menu.tsx';  // Импортируем компонент меню
+import { FaInstagram, FaWhatsapp, FaYoutube, FaPhone } from 'react-icons/fa';
+import { CiMail, CiSearch } from 'react-icons/ci';
+import { HiOutlineQuestionMarkCircle } from 'react-icons/hi';
+import { IoMdArrowDropdown } from 'react-icons/io';
 
 function Header() {
   const [isDestinationModalOpen, setDestinationModalOpen] = useState(false);
   const [isTourModalOpen, setTourModalOpen] = useState(false);
-
   const [menuOpen, setMenuOpen] = useState(false);
+  const [hideBurger, setHideBurger] = useState(false);
+
+  const destinationModalRef = useRef<HTMLDivElement>(null);
+  const tourModalRef = useRef<HTMLDivElement>(null);
+  const clickRef = useRef<HTMLDivElement>(null);
 
   const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
+    setMenuOpen(prev => {
+      if (!prev) {
+        // Если меню закрыто, открываем его и скрываем кнопку через 1 секунду
+        setHideBurger(false);
+        return true;
+      } else {
+        // Если меню открыто, сразу скрываем кнопку
+        setHideBurger(true);
+        setTimeout(() => setHideBurger(false), 1000); // Показываем кнопку через 1 секунду
+        return false;
+      }
+    });
   };
 
   const handleDestinationClick = () => {
@@ -28,65 +42,104 @@ function Header() {
     setDestinationModalOpen(false); // Закрыть направления, если открыто
   };
 
+  const handleClickOutside = (event: MouseEvent) => {
+    if (clickRef.current && !clickRef.current.contains(event.target as Node)) {
+      setDestinationModalOpen(false);
+      setTourModalOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <div className={styles.upperHeader}>
           <div className={styles.contactContainer}>
             <div className={styles.flex}>
-              <FaPhone/>
-              <a href="+996 700 01 55 99">+996 700 01 55 99</a>
+              <FaPhone />
+              <a href="tel:+996700015599">+996 700 01 55 99</a>
             </div>
             <div className={styles.flex2}>
-              <CiMail/>
+              <CiMail />
               <a href="mailto:Gocentralasia@gmail.com">Gocentralasia@gmail.com</a>
             </div>
             <div className={styles.flex3}>
-              <HiOutlineQuestionMarkCircle/>
+              <HiOutlineQuestionMarkCircle />
               <a href="#">FAQ</a>
             </div>
           </div>
           <div className={styles.socialContainer}>
-            <FaInstagram/>
-            <FaWhatsapp/>
-            <FaYoutube/>
+            <FaInstagram />
+            <FaWhatsapp />
+            <FaYoutube />
           </div>
         </div>
-        <div className={styles.header}>
-          <div className={styles.headerRow}>
-            <div className={styles.logoContainer}>
-              <img src={Logo} alt="logo"/>
-              <h2>Gocentralasia</h2>
-            </div>
-            <div className={styles.navigationContainer}>
-              <ul>
-                <li><a href="#">Home</a></li>
-                <li><span onClick={handleDestinationClick}>Destination <IoMdArrowDropdown/></span></li>
-                <li><span onClick={handleTourClick}>Tour <IoMdArrowDropdown/></span></li>
-                <li><a href="#">Blog</a></li>
-                <li><a href="#">About us</a></li>
-                <li><a href="#">Contact Us</a></li>
-              </ul>
-            </div>
-            <div className={styles.search_languageContainer}>
-              <div className={styles.searchContainer}>
-                <CiSearch className={styles.searchIcon}/>
+        <div className={styles.headerRow}>
+          <div className={styles.logoContainer}>
+            <img src={Logo} alt="logo" />
+            <h2>Gocentralasia</h2>
+          </div>
+          <div className={styles.navigationContainer}>
+            <ul>
+              <li><a className={styles.navigationLink} href="#">Home</a></li>
+              <li><span onClick={handleDestinationClick}>Destination <IoMdArrowDropdown /></span></li>
+              <li><span onClick={handleTourClick}>Tour <IoMdArrowDropdown /></span></li>
+              <li><a className={styles.navigationLink} href="#">Blog</a></li>
+              <li><a className={styles.navigationLink} href="#">About us</a></li>
+              <li><a className={styles.navigationLink} href="#">Contact Us</a></li>
+            </ul>
+            {(isDestinationModalOpen || isTourModalOpen) && (
+              <div className={styles.dropdownModals} ref={clickRef}>
+                {isDestinationModalOpen && (
+                  <div className={styles.dropdownModalDestination} ref={destinationModalRef}>
+                    <ul>
+                      <li><a className={styles.dropdownItem} href="#">Central Asia</a></li>
+                      <li><a className={styles.dropdownItem} href="#">Kyrgyzstan</a></li>
+                      <li><a className={styles.dropdownItem} href="#">Kazakhstan</a></li>
+                      <li><a className={styles.dropdownItem} href="#">Uzbekistan</a></li>
+                    </ul>
+                  </div>
+                )}
+                {isTourModalOpen && (
+                  <div className={styles.dropdownModalTour} ref={tourModalRef}>
+                    <ul>
+                      <li><a className={styles.dropdownItem} href="#">Adventure Tours</a></li>
+                      <li><a className={styles.dropdownItem} href="#">Cultural Tours</a></li>
+                      <li><a className={styles.dropdownItem} href="#">Nature Tours</a></li>
+                    </ul>
+                  </div>
+                )}
               </div>
-              <div className={styles.languageContainer}>
-                <h3>ENG</h3>
-                <IoMdArrowDropdown className={styles.dropDown}/>
-              </div>
+            )}
+          </div>
+          <div className={styles.searchLanguageContainer}>
+            <div className={styles.searchContainer}>
+              <CiSearch className={styles.searchIcon} />
             </div>
-            <div>
-              <button className={`${styles['burger-btn']} ${menuOpen ? styles['open'] : ''}`} onClick={toggleMenu}>
-                <span className={`${styles.line} ${menuOpen ? styles['line1'] : ''}`}></span>
-                <span className={`${styles.line} ${menuOpen ? styles['line2'] : ''}`}></span>
-                <span className={`${styles.line} ${menuOpen ? styles['line3'] : ''}`}></span>
-              </button>
+            <div className={styles.languageContainer}>
+              <h3>ENG</h3>
+              <IoMdArrowDropdown className={styles.dropDown} />
             </div>
           </div>
+          <div className={styles.mobileNav}>
+
+            <button
+              className={`${styles['burger-btn']} ${hideBurger ? styles['hidden'] : ''}`}
+              onClick={toggleMenu}
+            >
+              <span className={`${styles.line} ${menuOpen ? styles['line1'] : ''}`}></span>
+              <span className={`${styles.line} ${menuOpen ? styles['line2'] : ''}`}></span>
+              <span className={`${styles.line} ${menuOpen ? styles['line3'] : ''}`}></span>
+            </button>
+          </div>
         </div>
-        <Menu menuOpen={menuOpen} toggleMenu={toggleMenu}/>
+        <Menu menuOpen={menuOpen} toggleMenu={toggleMenu} />
       </div>
     </header>
   );
