e9a58f9e5d9d2e3cf79dcd5450d7c8fb884304c1
Aimejrh <ameerimzanbarakova@gmail.com>
2024-08-28 02:57:04 +0600

    GOC-7

---
+++ src/pages/ContactUsPage/index.tsx
@@ -0,0 +1,11 @@
+import styles from "../../globalStyle.module.css";
+import ContactUsCom from "./ContactUsCom/ContactUsCom.tsx";
+
+function Index() {
+    return (
+        <div className={styles.container}>
+           <ContactUsCom/>
+        </div>
+    );
+}
+export default Index
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,11 +2,15 @@ import {useRoutes} from 'react-router-dom';
 import Home from '../pages/Home';
 import DestinationPage from "../pages/DestinationPage";
 import ConditionsPage from "../pages/ConditionsPage";
+import TermsPrivacyPolicyPage from "../pages/TermsPrivacyPolicyPage";
+import ContactUsPage from "../pages/ContactUsPage";
 
 const routes = [
     {path: '/', element: <Home/>},
     {path: '/desitination', element: <DestinationPage/>},
     {path: '/conditions', element: <ConditionsPage/>},
+    {path: '/TermsPrivacyPolicy', element: <TermsPrivacyPolicyPage/>},
+    {path: '/ContactUs', element: <ContactUsPage/>},
 ];
 
 const RouterView = () => {
